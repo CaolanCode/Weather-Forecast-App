f80b47cd425bcaa3466c2326f8206c83d39d5dd7
CaolanCode <cmacmahon3@gmail.com>
2023-03-27 08:15:44 +0100

    config webpack

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/app.js')
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather Forecast'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(gif|png|jpg|jpeg|svg)$/i,
+        type: 'asset/resource'
+      }
+    ]
+  }
+}
